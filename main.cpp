54c68ada74142109f826e47e9573979952fbdcc1
syf <2654525303@qq.com>
2020-10-10 22:38:31 +0800

    add vec3

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int, char**) {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        std::cerr << "\rScanlines remaining: " << j << '\n' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
