622a1f8854934c0af48b79f3520af7e58e91b2e4
syf <2654525303@qq.com>
2020-10-15 22:07:08 +0800

    use custom classes to render image

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,22 @@
 #include <iostream>
+#include <fstream>
 
-#include "vec3.h"
+#include "rtxweekend.h"
+
 #include "color.h"
-#include "ray.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-double hit_sphere(const point3& center, double radius, const ray& r)
+color ray_color(const ray& r, const hittable& world)
 {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius * radius;
-    auto discriminant = half_b * half_b - a * c;
-    if (discriminant < 0)
+    hit_record record;
+    if (world.hit(r, 0, infinity, record))
     {
-        return -1.0;
-    }
-    else
-    {
-        return (-half_b - sqrt(discriminant)) / a; // pick the smaller root
-    }
-    
-    return discriminant > 0;
-}
-
-color ray_color(const ray& r)
-{
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0)
-    {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+        return 0.5 * (record.normal + color(1, 1, 1));
     }
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
-    return (1 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1, 1, 1) + t * color(0.5, 0.7, 1);
 }
 
 int main(int, char**) {
@@ -41,6 +24,12 @@ int main(int, char**) {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    std::ofstream out_image_file("C:\\Users\\syf\\Projects\\RTXInOneWeekend\\image.ppm");
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
     auto viewport_height = 2.0;
@@ -54,16 +43,16 @@ int main(int, char**) {
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    out_image_file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        std::cerr << "\rScan lines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto u = double(i) / (image_width - 1);
             auto v = double(j) / (image_height - 1);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r);
-            write_color(std::cout, pixel_color);
+            color pixel_color = ray_color(r, world);
+            write_color(out_image_file, pixel_color);
         }
     }
 
