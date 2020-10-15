7da4c15f63cd62775182e989fa14f3bef9aa672c
syf <2654525303@qq.com>
2020-10-15 15:53:47 +0800

    add front-face tracking to hit_record

--- src/Headers/hittable.h
+++ src/Headers/hittable.h
@@ -8,6 +8,13 @@ struct hit_record
     point3 p; // hit point
     vec3 normal;
     double t; // root of quadratic equation (used in ray::at)
+    bool front_face;
+
+    inline void set_face_normal(const ray& r, const vec3& outward_normal)
+    {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class hittable
